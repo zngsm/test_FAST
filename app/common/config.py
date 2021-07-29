dd0d29fec3aa26c9524926ac4b2e503ce6b4bfb2
zngsm <qbw0088@gmail.com>
2021-07-29 22:47:21 +0900

    [feat/setting] set config.py

--- app/common/config.py
+++ app/common/config.py
@@ -1,16 +1,55 @@
-# 환경별 변수
+# 환경별 변수 django의 settings.py ..!!
 # 서버는 운영 서버/ 개발 서버 / qa 수행하는 스테이징 서버도 있고, 로컬 환경도 있다.
 # 그때마다 다른 방법으로 설정파일 넣는 것으로 config.py에 하고 consts.py는 어떤 환경에도 변하지 않는 것
+
 
 from dataclasses import dataclass, asdict
 from os import path, environ
 
 base_dir = path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
 
+# dataclass 데코레이터 달고 있을 경우 해당 클래스를 dict 형태로 추추해서 쓸 수 있다.
 @dataclass
 class Config:
-
+    """
+    기본 configuration
+    """
     BASE_DIR = base_dir
 
     DB_POOL_RECYCLE: int = 900
     DB_ECHO: bool = True
+
+
+@dataclass
+class LocalConfig(Config):
+    PROJ_RELOAD: bool = True
+
+@dataclass
+class ProdConfig(Config):
+    PROJ_RELOAD: bool = False
+
+# print(LocalConfig().DB_ECHO)
+# True 출력
+
+def abc(DB_ECHO=None, DB_POOL_RECYCLE=None, **kwargs):
+    print(DB_ECHO, DB_POOL_RECYCLE)
+
+abc(LocalConfig())
+# LocalConfig(DB_POOL_RECYCLE=900, DB_ECHO=True, PROJ_RELOAD=True) None 출력
+
+print(asdict(LocalConfig()))
+# {'DB_POOL_RECYCLE': 900, 'DB_ECHO': True, 'PROJ_RELOAD': True}
+# 위의 abc를 이용해 출력했을 경우는 객체형태로 출력되지만, asdict로 출력하면 데코레이터를 붙여줬기 때문에 dict 형태로 출력할 수 있다.
+arg = asdict(LocalConfig())
+abc(arg)
+# {'DB_POOL_RECYCLE': 900, 'DB_ECHO': True, 'PROJ_RELOAD': True} None
+abc(**arg)
+# True 900
+
+def conf():
+    """
+    환경 불러오기
+    :return:
+    """
+    config = dict(prod=ProdConfig(), local=LocalConfig())
+    return config.get(environ.get("API_ENV", "local"))
--- app/main.py
+++ app/main.py
@@ -1,57 +1,29 @@
 from typing import Optional
 from fastapi import FastAPI
+import uvicorn
 
-app = FastAPI()
+from app.common.config import conf
 
+def create_app():
+    """
+    앱 함수 실행
+    :return:
+    """
+    c = conf()
+    app = FastAPI()
 
+    # 데이터베이스 이니셜 라이즈
+
+    # 레디스 이니셜라이즈
+
+    # 미들웨어 정의
+
+    # 라우터정의
+
+    return app
+
+app = create_app()
 # basic method
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
 
-# path parameter
-# if item_id got other type except int, it show type error (data validation check)
-# 127.0.0.1:8000/items/3
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return { "item_id" : item_id }
-
-
-# query parameter
-# if skip, limit got other type except int, it show type error (data validation check)
-# 127.0.0.1:8000/items/?skip=0&limit=0
-# if don't input value of skip and limit, it get fixed value 0 (cuz int=0)
-# but like 127.0.0.1:8000/items/?skip=10&limit=100, if they get other values, results are same with other values.
-# so int=0 <= it is default of query
-@app.get("/items/")
-def read_item_query(skip: int=0, limit: int=0):
-    return {"skip":skip, "limit":limit}
-# and get url query => q: ~~
-# 127.0.0.1:8000/items/3?q=hello => return {"item_id":3, "q": "hello" }
-
-# query Optional
-# q is just option, so don't input any query, its default is None.
-# item_id = url parameter , def_q = query, op_q = optional query
-# when I make path /items/3?def_q="hello"&op_q="hello2", function read_item's path is simiral with it
-# so can't redirect function read_item_params, modify path items => item
-@app.get("/item/{item_id}")
-def read_item_params(item_id: int, def_q: str='hello', op_q: Optional[str] = None):
-    return {"item_id": item_id, "def_q": def_q, "op_q": op_q}
-
-
-
-from pydantic import BaseModel
-
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
-
-# Body?!
-
-
-@app.post("/items/")
-async def create_item(item: Item):
-    item.name.
-    return item
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
